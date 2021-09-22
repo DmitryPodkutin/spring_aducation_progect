d2ff01efbc4325d3279d20447c1e8f783767fb47
Dmitry Podkutin <dmitry.podkutin@gmail.com>
2021-09-22 21:25:02 +0300

    Spring Boot - beginning Spring Boot JPA (Hibernate), Spring Boot Security

---
+++ src/main/java/com/example/servingwebcontent/controller/MainController.java
@@ -0,0 +1,58 @@
+package com.example.servingwebcontent.controller;
+
+import com.example.servingwebcontent.domain.Message;
+import com.example.servingwebcontent.repository.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class MainController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+        return "greeting";
+    }
+
+    @GetMapping("/main")
+    public String main(Map<String, Object> model) {
+        findAll(model);
+        return "main";
+    }
+
+    @PostMapping("/main")
+    String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+        findAll(model);
+        return "main";
+    }
+
+    @PostMapping("/filter")
+    public String filter(@RequestParam String tag, Map<String, Object> model) {
+        Iterable<Message> messages;
+        if (tag.isEmpty()) {
+            messages = messageRepo.findAll();
+        } else {
+            messages = messageRepo.findByTag(tag);
+        }
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping("/logout")
+    public String logout() {
+        return "/greeting";
+    }
+
+    private void findAll(Map<String, Object> model) {
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+    }
+}
---
+++ src/main/java/com/example/servingwebcontent/repository/UserRepo.java
@@ -0,0 +1,8 @@
+package com.example.servingwebcontent.repository;
+
+import com.example.servingwebcontent.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
